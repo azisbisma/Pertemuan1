9029eeed596b4e4b14810ff90d47fb91b8744543
Azis Bisma Yanuarta <41156131+azisbisma@users.noreply.github.com>
2023-08-23 15:18:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Pertemuan 1</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+	<div class="container p-5 my-5 shadow-lg p-3 mb-5 bg-body rounded">
+		<h1 class="text-center">Introduce My Self</h1>
+	</div>
+	<div class="container p-5 my-5 shadow-lg p-3 mb-5 bg-body rounded">
+		<p class="text-start fs-4">Azis Bisma Yanuarta</p>
+		<p class="text-start fs-4">Universitas Muhammadiyah Surakarta</p>
+		<p class="text-start fs-4">Informatics Engineering</p>
+	</div>
+</body>
+
+</html>
